6f11a209b92ea71817dac382174c5e6f3f1bd13f
james afolaranmi <olaoluwaafolaranmi@gmail.com>
2020-07-30 09:23:56 +0100

    wave 1 labs

---
+++ Lab1of9.py
@@ -0,0 +1,10 @@
+# Browse the complete list of string methods at:
+# https://docs.python.org/3/library/stdtypes.html#string-methods 
+#and try them out here. Endeavor to copy the link without the first # comment sign beginning it.
+
+word = "i am very hungry"
+reply = "let us go to the restaurant"
+#to check if all characters in word are in lower case
+print(word.islower())
+#to join word and reply together
+print(".".join([word,reply]))
---
+++ Lab2of1.py
@@ -0,0 +1,7 @@
+# Write two lines of code below, each assigning a value to a variable
+y = 9
+x = 8
+
+# Now write a print statement using .format() to print out a sentence and the 
+#   values of both of the variables
+print("the sum of x and y is {}".format(x+y))
